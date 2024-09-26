280b1ec0879f37f560e9d1afee743ef4d9e1e28a
Gunda Karthik <gundakarthik644@gmail.com>
2024-09-26 11:09:10 +0530

    Initialized the project

---
+++ src/main/java/com/scm/scm2_0/Scm20Application.java
@@ -0,0 +1,12 @@
+package com.scm.scm2_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scm20Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Scm20Application.class, args);
+        System.out.println("Working....");
+    }
+}
---
+++ src/test/java/com/scm/scm2_0/Scm20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.scm2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scm20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
